26a11554ce8225617efb4f9b43d885d0be4d7b38
luisgnacio123@gmail.com <luisgnacio123@gmail.com>
2024-07-14 11:03:31 -0400

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+// Register
 async function alertMessege() {
-    const { value: formValues } = await Swal.fire({
+    const {
+        value: formValues
+    } = await Swal.fire({
         title: 'Registro',
         html: `
             <div style="text-align: left; width: 100%;">
@@ -29,8 +32,11 @@ async function alertMessege() {
     }
 }
 
+// Login
 async function alertLogin() {
-    const { value: formValues } = await Swal.fire({
+    const {
+        value: formValues
+    } = await Swal.fire({
         title: 'Iniciar sesion',
         html: `
             <div style="text-align: left; width: 100%;">
@@ -41,7 +47,7 @@ async function alertLogin() {
             </div>
         `,
         focusConfirm: false,
-        confirmButtonText: 'Registrar',
+        confirmButtonText: 'Iniciar Sesión',
         customClass: {
             confirmButton: 'custom-confirm'
         },
@@ -57,3 +63,166 @@ async function alertLogin() {
         Swal.fire(`Nombre: ${formValues[0]}, Correo: ${formValues[1]}`);
     }
 }
+
+// Add to shopping cart 
+
+async function addShoppingCart() {
+    Swal.fire({
+        title: "Agregado al carrito",
+        icon: "success"
+    });
+}
+
+// Purshase - only example 
+async function purshase() {
+    Swal.fire({
+        title: 'Método de Pago',
+        html: `
+          <div style="text-align: left; width: 100%;">
+            <label for="swal-input1">Nombre del Titular:</label>
+            <input id="swal-input1" class="swal2-input" placeholder="Nombre completo">
+            <label for="swal-input2">Número de Tarjeta:</label>
+            <input id="swal-input2" class="swal2-input" placeholder="XXXX-XXXX-XXXX-XXXX" maxlength="16">
+            <label for="swal-input3">Fecha de Vencimiento:</label>
+            <input id="swal-input3" class="swal2-input" placeholder="MM/AA" maxlength="5">
+            <label for="swal-input4">CVV:</label>
+            <input id="swal-input4" class="swal2-input" placeholder="XXX" maxlength="3" type="password">
+          </div>
+        `,
+        focusConfirm: false,
+        confirmButtonText: 'Confirmar pago',
+        customClass: {
+            confirmButton: 'custom-confirm'
+        },
+        preConfirm: () => {
+            return [
+                document.getElementById('swal-input1').value,
+                document.getElementById('swal-input2').value,
+                document.getElementById('swal-input3').value,
+                document.getElementById('swal-input4').value
+            ];
+        }
+    }).then((formResult) => {
+        if (formResult.value) {
+            Swal.fire({
+                title: "¿Quieres guardar metodo de pago?",
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: "Guardar",
+                denyButtonText: `No guardar`
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire("¡Pago procesado!", "", "success");
+                } else if (result.isDenied) {
+                    Swal.fire("¡Pago procesado! Los cambios no se han guardado", "", "info");
+                }
+            });
+        }
+    });
+}
+
+
+// transicion de imagenes home
+document.addEventListener("DOMContentLoaded", function () {
+    const images = [
+        "images/books/sherlockHolmes.jpg",
+        "images/books/theLordRings.jpg",
+        "images/books/leyesIndispensables.jpg",
+        "images/books/sexoAmorNoviazgo.jpg",
+        "images/books/cienAniosDeSoledad.jpg",
+        "images/books/harryPotter.jpg",
+        "images/books/power.jpg"
+    ];
+
+    const titles = [
+        "Las Aventuras de Sherlock Holmes",
+        "El Señor de los Anillos",
+        "Las 15 Leyes Indispensables del Crecimiento",
+        "Amor, Sexo y Noviazgo",
+        "Cien Años de Soledad",
+        "Harry Potter: El legado Oscuro",
+        "Las 48 Leyes del Poder"
+    ];
+
+    const authors = [
+        "Arthur Conan Doyle",
+        "J.R.R. Tolkien",
+        "John C. Maxwell",
+        "Timothy Keller",
+        "Gabriel García Márquez",
+        "J.K. Rowling",
+        "Eckhart Tolle"
+    ];
+
+    const descriptions = [
+        "Las Aventuras de Sherlock Holmes es una colección de doce relatos cortos escritos por Arthur Conan Doyle, que presenta las ingeniosas y cautivadoras historias del detective más famoso del mundo, Sherlock Holmes, y su fiel amigo, el Dr. John Watson.",
+        "El Señor de los Anillos es una novela de fantasía escrita por el filólogo y escritor británico J. R. R. Tolkien.",
+        "Las 15 Leyes Indispensables del Crecimiento es un libro de John C. Maxwell que define las leyes esenciales para el crecimiento que usted debe seguir.",
+        "Amor, Sexo y Noviazgo es un libro de Sixto Porras que aborda las relaciones amorosas desde una perspectiva cristiana.",
+        "Cien Años de Soledad es una novela escrita por el colombiano Gabriel García Márquez, considerada una obra maestra de la literatura hispanoamericana y universal.",
+        "Harry Potter es una serie de novelas fantásticas escritas por la autora británica J. K. Rowling.",
+        "Las 48 Leyes del Poder es un libro de Robert Greene que nos enseña sobre la estrategia, el poder, la seducción, la maestría de nuestras habilidades y las motivaciones psicológicas, sociales y culturales que guían nuestras decisiones cada día."
+    ];
+
+    let currentImageIndex = 0;
+    const imageContainer = document.querySelector(".image__container");
+    let imgElement = document.createElement("img");
+    imgElement.src = images[currentImageIndex];
+    imgElement.classList.add("home__contenido__imagen", "active");
+    imageContainer.appendChild(imgElement);
+
+    const titleElement = document.querySelector(".home__conteindo__titulo");
+    const authorElement = document.querySelector(".home__contenido__autor");
+    const descriptionTitleElement = document.querySelector(".home__contenido__titulo__descripcion");
+    const descriptionElement = document.querySelector(".home__contenido__descripcion");
+
+    titleElement.textContent = titles[currentImageIndex];
+    authorElement.textContent = `Autor: ${authors[currentImageIndex]}`;
+    descriptionTitleElement.textContent = titles[currentImageIndex];
+    descriptionElement.textContent = descriptions[currentImageIndex];
+
+    // Inicializa los textos con la clase fade y active
+    titleElement.classList.add("fade", "active");
+    authorElement.classList.add("fade", "active");
+    descriptionTitleElement.classList.add("fade", "active");
+    descriptionElement.classList.add("fade", "active");
+
+    const updateContent = () => {
+        titleElement.classList.remove("active");
+        authorElement.classList.remove("active");
+        descriptionTitleElement.classList.remove("active");
+        descriptionElement.classList.remove("active");
+
+        setTimeout(() => {
+            titleElement.textContent = titles[currentImageIndex];
+            authorElement.textContent = `Autor: ${authors[currentImageIndex]}`;
+            descriptionTitleElement.textContent = titles[currentImageIndex];
+            descriptionElement.textContent = descriptions[currentImageIndex];
+
+            titleElement.classList.add("active");
+            authorElement.classList.add("active");
+            descriptionTitleElement.classList.add("active");
+            descriptionElement.classList.add("active");
+        }, 50); // Sincronizar con el retraso de la clase active
+    };
+
+    setInterval(() => {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        const newImgElement = document.createElement("img");
+        newImgElement.src = images[currentImageIndex];
+        newImgElement.classList.add("home__contenido__imagen");
+        imageContainer.appendChild(newImgElement);
+
+        setTimeout(() => {
+            newImgElement.classList.add("active");
+            imgElement.classList.remove("active");
+
+            setTimeout(() => {
+                imageContainer.removeChild(imgElement);
+                imgElement = newImgElement;
+            }, 1000); // Duración de la transición en ms
+        }, 50); // Pequeño retraso para asegurar que la clase active se añade correctamente
+
+        updateContent();
+    }, 5000); // Cambia la imagen cada 5 segundos
+});
